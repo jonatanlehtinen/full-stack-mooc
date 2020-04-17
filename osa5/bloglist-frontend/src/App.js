832ce2c708668f357ff4d738d118ecb2f55bb6bc
Jonatan Lehtinen <=>
2020-04-17 15:02:47 +0300

    5.1-5.4

--- osa5/bloglist-frontend/src/App.js
+++ osa5/bloglist-frontend/src/App.js
@@ -1,9 +1,20 @@
 import React, { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/loginService'
+import Notification from './components/Notification'
+
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('') 
+  const [password, setPassword] = useState('') 
+  const [title, setTitle] = useState('') 
+  const [author, setAuthor] = useState('') 
+  const [url, setURL] = useState('') 
+  const [user, setUser] = useState(null)
+  const [notification, setNotification] = useState(null)
+
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -11,14 +22,137 @@ const App = () => {
     )  
   }, [])
 
-  return (
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+  }, [])
+
+  const showNotification = (message, successful) => {
+    const newNotification = {message, successful}
+    setNotification(newNotification)
+    setTimeout(() => {
+      setNotification(null)
+    }, 5000)
+  }
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    try {
+      const user = await loginService.login({
+        username, password,
+      })
+
+      window.localStorage.setItem(
+        'loggedBlogUser', JSON.stringify(user)
+      ) 
+
+      setUser(user)
+      setUsername('')
+      setPassword('')
+      showNotification(`${user.name} logged in successfully`, true)
+    } catch (exception) {
+      showNotification("Wrong username or password", false)
+    }
+  }
+
+  const handleLogout = () => {
+    window.localStorage.removeItem('loggedBlogUser')
+    setUser(null)
+  }
+
+  const handleCreateBlog = async (event) => {
+    event.preventDefault()
+    const newBlog = { title, author, url }
+    try {
+      const response = await blogService.create(newBlog)
+      const updatedBlogs = blogs.concat(response)
+      setBlogs(updatedBlogs)
+      showNotification(`A new blog ${title} by ${author} added`, true)
+    } catch(exception) {
+      showNotification(`Failed to add new blog`, false)
+    }
+  }
+
+  const loginForm = () => (
     <div>
+      <Notification notification={notification}/>
+      <h2>Login</h2>
+    
+      <form onSubmit={handleLogin}>
+        <div>
+          username
+            <input
+            type="text"
+            value={username}
+            name="Username"
+            onChange={({ target }) => setUsername(target.value)}
+          />
+        </div>
+        <div>
+          password
+            <input
+            type="password"
+            value={password}
+            name="Password"
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </div>
+        <button type="submit">login</button>
+      </form>
+    </div>
+  )
+  
+
+  const content = () => (
+    <div>
+      <Notification notification={notification}/>
       <h2>blogs</h2>
+      <p>{user.name} logged in <button onClick={handleLogout}>logout</button></p>
+      <h2>create new</h2>
+          
+      <form onSubmit={handleCreateBlog}>
+        <div>
+          title:
+            <input
+            type="text"
+            value={title}
+            name="Title"
+            onChange={({ target }) => setTitle(target.value)}
+          />
+        </div>
+        <div>
+          author:
+            <input
+            type="text"
+            value={author}
+            name="Author"
+            onChange={({ target }) => setAuthor(target.value)}
+          />
+        </div>
+        <div>
+          url:
+            <input
+            type="text"
+            value={url}
+            name="URL"
+            onChange={({ target }) => setURL(target.value)}
+          />
+        </div>
+        <button type="submit">create</button>
+      </form>
+    
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
     </div>
   )
+
+  return user === null ? loginForm() : content()
+  
 }
 
 export default App
---
+++ osa5/bloglist-frontend/src/components/Notification.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+import "../styles/styles.css"
+
+const Notification = ({ notification }) => {
+  if(notification === null) {
+    return null
+  }
+
+  return (
+    <div className={notification.successful ? "success" : "error"}>
+      {notification.message}
+    </div>
+  )
+}
+
+export default Notification
